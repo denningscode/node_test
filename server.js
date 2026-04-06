bf1d9f723b71e7042063b664e234c1e05748b5e4
Denning Kahky <35266252+denningscode@users.noreply.github.com>
2026-04-06 14:45:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 
-const port = 3000;
+const port = Number(process.env.PORT || 3000);
 
 app.get("/", (req, res) => {
     res.send("Server is up and running on " + port)
